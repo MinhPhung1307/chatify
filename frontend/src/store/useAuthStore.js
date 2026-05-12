9618b7fce8def3326f4df2cadbd942c5d3a14b76
Nguyen Minh Phung <nguyenminhphung1307@gmail.com>
2026-05-12 10:39:23 +0700

    update BASE_URL

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from '../lib/axios';
 import toast from 'react-hot-toast';
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://chatify-kiw8.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
@@ -87,6 +87,7 @@ export const useAuthStore = create((set, get) => ({
 
         const socket = io(BASE_URL, {
             withCredentials: true, // this ensures cookies are sent with the connection
+            transports: ["websocket", "polling"],
         });
 
         socket.connect();
